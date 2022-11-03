181ceb5b191be48165c30641f60242ee4feee9fb
oscar <97479266+o-scarzhu@users.noreply.github.com>
2022-11-03 13:52:04 -0700

    Hello World

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject.comp2522202230termprojectchadarcade {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens ca.bcit.comp2522.termproject.comp2522202230termprojectchadarcade to javafx.fxml;
+  exports ca.bcit.comp2522.termproject.comp2522202230termprojectchadarcade;
+}
