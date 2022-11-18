4a6499e2c2c5d46897c4fe81c2a5df9e33ca7217
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-11-17 17:59:10 -0800

    implmeneted map

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadarcade/Platformer.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadarcade/Platformer.java
@@ -1,59 +1,58 @@
 package ca.bcit.comp2522.termproject.comp2522202230termprojectchadarcade;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.dyn4j.dynamics.Body;
+import org.dyn4j.geometry.AABB;
 import org.dyn4j.geometry.Geometry;
 import org.dyn4j.geometry.MassType;
 import org.dyn4j.world.World;
+import org.tiledreader.*;
 
 
-
-public class Platformer {
-    World<Body> stage = new World<>();
+public class Platformer extends Application {
+    TiledReader tileFile = new FileSystemTiledReader();
+    private final World<Body> stage = new World<>();
     private Double x;
     private Double y;
-
     public Platformer(){
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        initializeWorld();
+        Group group = new Group();
+        Scene scene = new Scene(group);
+        stage.setHeight(500);
+        stage.setWidth(500);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
     private static final Object CHARACTER = new Object();
     private static final Object FLOOR = new Object();
 
     protected void initializeWorld(){
-        //create floor
-        Body floor = new Body();
-        floor.addFixture(Geometry.createRectangle(50,0.2));
-        floor.setMass(MassType.INFINITE);
-        floor.translate(0, -6);
-        floor.setUserData(FLOOR);
-        stage.addBody(floor);
-
-        // make character and put it on stage
-        Body body = new Body();
-        body.addFixture(Geometry.createRectangle(0.3, 1.7));
-        body.setMass(MassType.NORMAL);
-        body.translate(0,50);
-        body.setUserData(CHARACTER);
-        stage.addBody(body);
-
-        // make right side of stage
-        Body right = new Body();
-        right.addFixture(Geometry.createRectangle(0.2, 25.7));
-        right.setMass(MassType.INFINITE);
-        right.translate(10, 7);
-        stage.addBody(right);
-
-        // make left side of stage
-        Body left = new Body();
-        left.addFixture(Geometry.createRectangle(0.2, 25.7));
-        left.setMass(MassType.INFINITE);
-        left.translate(-10, 7);
-        stage.addBody(left);
-    }
-
-    public final double getY(){
-        return this.y == null ? 0.0 : this.y.get();
+        TiledMap tiledMap = tileFile.getMap(HelloApplication.class.getResource("/collision.tmx").getPath());
+        for(TiledLayer layer: tiledMap.getTopLevelLayers()){
+            if(layer instanceof TiledObjectLayer){
+                TiledObjectLayer tileObj =  (TiledObjectLayer) layer;
+                for(TiledObject tiledObject: tileObj.getObjects()) {
+                    Body body = new Body();
+                    body.addFixture(Geometry.createRectangle(tiledObject.getWidth(), tiledObject.getHeight()));
+                    body.translate(tiledObject.getX(), tiledObject.getY());
+                    body.setMass(MassType.INFINITE);
+                    stage.addBody(body);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
+        launch();
     }
-
 }
