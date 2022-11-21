28659c37cdf30ada90a48bf148762e19ac11499c
Oscar Zhu <ozhu35@gmail.com>
2022-11-20 18:21:56 -0800

    feat: add enterbutton method

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/Controller.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/Controller.java
@@ -1,4 +1,15 @@
 package ca.bcit.comp2522.termproject.comp2522202230termprojectchadclimbers;
 
+import javafx.event.ActionEvent;
+
+/**
+ * Handles all events for the fxml files.
+ * @author Oscar
+ * @version 2022
+ */
 public class Controller {
+  public void enterButton(ActionEvent event) {
+    // Placeholder.
+    System.out.println(event);
+  }
 }
