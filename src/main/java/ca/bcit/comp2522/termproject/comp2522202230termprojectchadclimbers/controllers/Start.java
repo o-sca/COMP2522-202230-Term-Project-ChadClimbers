b41606da5146188265bc27564b23c1d1a325985a
oscar <97479266+o-scarzhu@users.noreply.github.com>
2022-11-22 01:53:15 -0800

    refactor(start): minimise redundant code

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/controllers/Start.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/controllers/Start.java
@@ -20,18 +20,37 @@ public class Start extends Controller {
   @FXML
   private AnchorPane rootPane;
 
-  public void playerButton(ActionEvent event) throws IOException {
-    AnchorPane pane = FXMLLoader.load(ChadClimbers.class.getResource("Player.fxml"));
+  /**
+   * Loads FXML file and displays a subscene-like pane.
+   * TO DO: Maybe move this method to Controller class.
+   * @param fileName FXML File Name
+   * @throws IOException if FXML file is not found.
+   */
+  private void setPane(String fileName) throws IOException {
+    AnchorPane pane = FXMLLoader.load(ChadClimbers.class.getResource(fileName + ".fxml"));
     rootPane.getChildren().setAll(pane);
   }
 
-  public void stageButton(ActionEvent event) throws IOException {
-    AnchorPane pane = FXMLLoader.load(ChadClimbers.class.getResource("SelectStage.fxml"));
-    rootPane.getChildren().setAll(pane);
+  /**
+   * Invokes setPane method with the Player FXML file.
+   * @throws IOException if FXML file is not found.
+   */
+  public void playerButton() throws IOException {
+    setPane("Player");
   }
 
-  public void levelButton(ActionEvent event) throws IOException{
-    AnchorPane pane = FXMLLoader.load(ChadClimbers.class.getResource("Level.fxml"));
-    rootPane.getChildren().setAll(pane);
+  /**
+   * Invokes setPane method with the SelectStage FXML file.
+   * @throws IOException if FXML file is not found.
+   */
+  public void stageButton() throws IOException {
+    setPane("SelectStage");
+  }
+  /**
+   * Invokes setPane method with the Level FXML file.
+   * @throws IOException if FXML file is not found.
+   */
+  public void levelButton() throws IOException{
+    setPane("Level");
   }
 }
