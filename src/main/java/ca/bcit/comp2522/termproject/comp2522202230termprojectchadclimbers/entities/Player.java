4af991d794a72d174cec71e9a790e7eb74d75e8d
oscar <97479266+o-scarzhu@users.noreply.github.com>
2022-11-25 14:59:03 -0800

    refactor(player): change class to enum

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/entities/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/entities/Player.java
@@ -1,7 +1,17 @@
 package ca.bcit.comp2522.termproject.comp2522202230termprojectchadclimbers.entities;
 
-public class Player {
-  public Player() {
+import ca.bcit.comp2522.termproject.comp2522202230termprojectchadclimbers.ChadClimbers;
 
+public enum Player {
+  GIRL(ChadClimbers.class.getResource("entities/player1.gif").toString());
+
+  private final String playerURL;
+
+  private Player(String playerURL) {
+    this.playerURL = playerURL;
+  }
+
+  public String getURL() {
+    return playerURL;
   }
 }
