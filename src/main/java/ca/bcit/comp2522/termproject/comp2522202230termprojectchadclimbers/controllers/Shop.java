9d148855333bc995d51bfaec1c7a1ac61b91ec19
oscar <97479266+o-sca@users.noreply.github.com>
2022-12-02 00:01:54 -0800

    feat: create shop controller

---
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202230termprojectchadclimbers/controllers/Shop.java
@@ -0,0 +1,9 @@
+package ca.bcit.comp2522.termproject.comp2522202230termprojectchadclimbers.controllers;
+
+/**
+ * Represents the Shop class.
+ * @author Oscar
+ * @version 2022
+ */
+public class Shop extends Controller {
+}
